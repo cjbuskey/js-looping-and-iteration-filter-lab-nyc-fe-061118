6ec47a656d38c5fa5b2099ba97f362efe8e35f9a
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-26 00:33:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(driver, names) {
+  return driver.filter(driver => driver.toLowerCase() == names.toLowerCase());
+}
+
+function fuzzyMatch(driver, names) {
+  return driver.filter(driver => driver.slice(0,names.length) == names);
+}
+
+function matchName(driver, names) {
+  return driver.filter(driver => driver.name == names)
+}
