fefab707f829a35b97a6f74ba41ed7290a4bb678
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-26 00:29:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function findMatching(driver, names) {
 }
 
 function fuzzyMatch(driver, names) {
-  return driver.filter(driver => driver.slice(0,name.length) == names);
+  return driver.filter(driver => driver.slice(0,names.length) == names);
 }
